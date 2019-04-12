bc1f58a2a6d1c523c89d72f8da2b0cec31c02056
gerry5 <35868886+gerry5@users.noreply.github.com>
2019-04-12 23:17:37 +0800

    Update gjrj.sh

--- jrj_scrapy/gjrj.sh
+++ jrj_scrapy/gjrj.sh
@@ -1,3 +1,15 @@
-#!/usr/bin/sh
+#!/bin/bash
 
+venv/bin/pip3 list | grep PyMySQL > /dev/null
+if [ $? -eq 0 ]
+then
+    echo "PyMySQL Installed. "
+else
+    echo "Installing PyMySQL..."
+    venv/bin/pip3 install pymysql > /dev/null
+    echo "[ OK ]"
+fi
+
+
+# 运行
 venv/bin/scrapy crawl jrj --nolog
